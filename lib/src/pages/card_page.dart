93a3e39ecad20e7f125cf664a0e18d01c25e58c5
leozero3 <realleo2009@hotmail.com>
2021-04-30 21:38:06 -0300

    18

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -11,6 +11,8 @@ class CardPage extends StatelessWidget {
         padding: EdgeInsets.all(10),
         children: [
           _cartType1(),
+          SizedBox(height: 30),
+          _cardType2(),
         ],
       ),
     );
@@ -32,10 +34,39 @@ Widget _cartType1() {
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FlatButton(onPressed: (){}, child: Text('OK')),
-            FlatButton(onPressed: (){}, child: Text('Cancel'))
+            FlatButton(
+              onPressed: () {},
+              child: Text('OK'),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Cancel'),
+            ),
           ],
         )
+      ],
+    ),
+  );
+}
+
+Widget _cardType2() {
+  return Card(
+    child: Column(
+      children: [
+        FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT54P9IHux9WLy7'
+              'uo8easFiuawlZ24dhMpWwQ&usqp=CAU'),
+          height: 300,
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+
+        // Image(
+        //   image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT54P9IHux9WLy7'
+        //       'uo8easFiuawlZ24dhMpWwQ&usqp=CAU'),
+        // ),
+        Container(padding: EdgeInsets.all(10), child: Text('uma descrição da imagem'))
       ],
     ),
   );
