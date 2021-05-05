d9139d1fdd5a255a2b004d2dd120bcc3148a2e71
leozero3 <realleo2009@hotmail.com>
2021-05-04 23:14:10 -0300

    26

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:componentes_flutter/src/pages/alert_page.dart';
 import 'package:componentes_flutter/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,18 +9,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Componentes App',
-      //home: HomePage(),
-        initialRoute: '/',
-
-    routes: getApplicationRoutes(),
-    onGenerateRoute: (settings){
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('pt', 'BR'), // English, no country code
+        //const Locale('es', ''), // Spanish, no country code
+      ],
+      initialRoute: '/',
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (settings) {
         print('rotas: ${settings.name}');
 
         return MaterialPageRoute(builder: (context) => AlertPage());
-
-    },
+      },
     );
   }
 }
--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class InputPage extends StatefulWidget {
   const InputPage({Key key}) : super(key: key);
 
@@ -8,6 +9,12 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  String _name = '';
+  String _email = '';
+  String _date = '';
+
+  TextEditingController _inputFieldDateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +25,109 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         children: [
           _createInput(),
+          Divider(),
+          _createEmail(),
+          Divider(),
+          _createPassword(),
+          Divider(),
+          createDate(context),
+          Divider(),
+          _createPerson(),
         ],
       ),
     );
   }
 
-  Widget _createInput(){
+  Widget _createInput() {
     return TextField(
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
-        counter: Text('letras 0'),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        counter: Text('letras ${_name.length}'),
         hintText: 'Nome da Pessoa',
-        labelText: 'Nome'
+        labelText: 'Nome',
+        helperText: 'Somente o Nome',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
       ),
-
+      onChanged: (value) {
+        setState(() {
+          _name = value;
+        });
+      },
     );
+  }
+
+  Widget _createPerson() {
+    return ListTile(
+      title: Text('O nome é: $_name'),
+      subtitle: Text('Email: $_email'),
+    );
+  }
+
+  _createEmail() {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          hintText: 'Email',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email),
+        ),
+        onChanged: (value) => setState(() {
+              _email = value;
+            }));
+  }
+
+  _createPassword() {
+    return TextField(
+        enableInteractiveSelection: false,
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+          hintText: 'Senha',
+          labelText: 'Senha',
+          suffixIcon: Icon(Icons.lock_open),
+          icon: Icon(Icons.lock),
+        ),
+        onChanged: (value) => setState(() {
+              _email = value;
+            }));
+  }
+
+  createDate(BuildContext context) {
+    return TextField(
+      controller: _inputFieldDateController,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        hintText: 'Data de nascimento',
+        labelText: 'Data de nascimento',
+        suffixIcon: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today),
+      ),
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  void _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2021),
+        lastDate: DateTime(2031),
+      locale: Locale('pt', 'BR')
+    );
+    
+    if (picked != null) {
+      setState(() {
+        _date = picked.toString();
+        _inputFieldDateController.text = _date;
+      });
+    }  
   }
 }
